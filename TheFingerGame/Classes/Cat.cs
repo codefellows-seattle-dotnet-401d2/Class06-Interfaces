8d185c130f695c427e21e6ea52cbd969ca992e37
Dustin Mundy <Dustin.A.Mundy@gmail.com>
2017-12-29 14:02:11 -0800

    Added The Great Race!

---
+++ TheFingerGame/Classes/Cat.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TheFingerGame.Classes.Interfaces;
+
+namespace TheFingerGame.Classes
+{
+
+    class Cat : IMoveFast, IMoveSlow
+    {
+        public int Distance { get; set; }
+
+        public void DecideSpeed()
+        {
+            Random rand = new Random();
+            int laziness = rand.Next(0, 2);
+            switch (laziness)
+            {
+                case 0:
+                    MoveFast();
+                    break;
+                case 1:
+                    MoveSlow();
+                    break;
+                case 2:
+                    Console.Write("The Cat took a nap.");
+                    break;
+            }
+            Console.WriteLine(" Cat is " + Distance + " meters from the finish line.");
+        }
+
+        public void MoveFast()
+        {
+            Random rand = new Random();
+            int speed = rand.Next(3, 5);
+            Distance = Distance - speed;
+            Console.WriteLine("The Cat chased a bug for a bit.");
+        }
+
+        public void MoveSlow()
+        {
+            Random rand = new Random();
+            int speed = rand.Next(1, 3);
+            Distance = Distance - speed;
+            Console.WriteLine("The Cat wandered towards the finishline.");
+        }
+    }
+}
---
+++ TheFingerGame/Classes/Tortoise.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TheFingerGame.Classes.Interfaces;
+
+namespace TheFingerGame.Classes
+{
+    class Tortoise : IMoveSlow
+    {
+        public int Distance { get; set; }
+
+        public void MoveSlow()
+        {
+            Random rand = new Random();
+            int speed = rand.Next(2, 3);
+            Distance = Distance - speed;
+            Console.Write("The Tortoise powered forward.");
+            Console.WriteLine(" Tortoise is " + Distance + " meters from the finish line.");
+        }
+    }
+}
