33adb9a02c480a9750e95559023ad3199f400640
Luay <luayghazwan@yahoo.com>
2018-01-01 13:33:02 -0800

    Create Players to start fighting

--- MortalKombatConsoleGame/MortalKombatConsoleGame/Program.cs
+++ MortalKombatConsoleGame/MortalKombatConsoleGame/Program.cs
@@ -1,9 +1,14 @@
 ﻿using System;
+using MortalKombatConsoleGame.Classes;
 
 namespace MortalKombatConsoleGame
 {
     class Program
     {
+        /// <summary>
+        /// Main entry point for the application
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             Console.WriteLine("\n Welcome to the simplest Mortal Kombat " +
@@ -20,13 +25,34 @@ namespace MortalKombatConsoleGame
             bool gameOn = true;
             while (gameOn)
             {
-                gameOn = StartFight();
+                gameOn = CreatePlayers();
             }
         }
 
-        public static bool StartFight()
+        public static bool CreatePlayers()
         {
-            
+            Console.WriteLine(" Enter the first Character/Player Name ");
+            string nameOne = Console.ReadLine();
+
+            Console.WriteLine(" Enter the first player Damage per attack points from 1 - 10 ");
+            string damagePlayerOne = Console.ReadLine();
+            int damageIntOne = int.Parse(damagePlayerOne);
+
+            PlayerOne playerOne = new PlayerOne(nameOne, damageIntOne);
+
+            Console.WriteLine(" Enter the second Character/Player Name ");
+            string nameTwo = Console.ReadLine();
+
+            PlayerTwo playerTwo = new PlayerTwo(nameTwo);
+
+            Console.WriteLine(" Do you want to keep fighting? Type 'Y' for Yes or 'N' for No");
+            string keepFighting = Console.ReadLine();
+            char keepFightingChar = char.Parse(keepFighting);
+
+            bool fightOn;
+            fightOn = char.ToUpper(keepFightingChar) == 'Y';
+
+            return fightOn;
         }
     }
 }
