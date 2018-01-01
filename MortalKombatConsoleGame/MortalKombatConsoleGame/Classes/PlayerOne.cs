c5cffd80f6213b9def08eb791acd8c3c3e8ffe1d
Luay <luayghazwan@yahoo.com>
2017-12-31 18:55:27 -0800

    Player one implement base class and IAttacker interface

---
+++ MortalKombatConsoleGame/MortalKombatConsoleGame/Classes/PlayerOne.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MortalKombatConsoleGame.Interfaces;
+
+namespace MortalKombatConsoleGame.Classes
+{
+    class PlayerOne : Player, IAttacker
+    {
+        public int Punch()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Kick()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
