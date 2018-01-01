30e1f45b58bc924c429f1e6e5364691ba3a1b9aa
Luay <luayghazwan@yahoo.com>
2017-12-31 18:58:26 -0800

    Player two implements its base abstract class and IDefender

---
+++ MortalKombatConsoleGame/MortalKombatConsoleGame/Classes/PlayerTwo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MortalKombatConsoleGame.Interfaces;
+
+namespace MortalKombatConsoleGame.Classes
+{
+    class PlayerTwo : Player, IDefender
+    {
+        public bool TakeCover()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Hurt(int attackType)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
