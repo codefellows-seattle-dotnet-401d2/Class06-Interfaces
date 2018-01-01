a311c5d3990802621647a83f83fd338d2e214a3a
Luay <luayghazwan@yahoo.com>
2017-12-31 18:52:45 -0800

    Player implementing Iplayer

--- MortalKombatConsoleGame/MortalKombatConsoleGame/Classes/Player.cs
+++ MortalKombatConsoleGame/MortalKombatConsoleGame/Classes/Player.cs
@@ -1,13 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using MortalKombatConsoleGame.Interfaces;
 
 namespace MortalKombatConsoleGame.Classes
 {
-    public abstract class Player
+    public abstract class Player : IPlayer
     {
         public int CharacterNumber { get; set; }
         public int Health { get; set; }
         public int Rank { get; set; }
+        public void Walk()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Run()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Jump()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Eat(int food)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
