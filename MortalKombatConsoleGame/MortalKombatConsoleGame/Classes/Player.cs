540be5f8a7f8d98ca54e0fa4fa8c6c9e20cfe342
Luay <luayghazwan@yahoo.com>
2017-12-31 18:49:46 -0800

    Player abstract class

---
+++ MortalKombatConsoleGame/MortalKombatConsoleGame/Classes/Player.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MortalKombatConsoleGame.Classes
+{
+    public abstract class Player
+    {
+        public int CharacterNumber { get; set; }
+        public int Health { get; set; }
+        public int Rank { get; set; }
+    }
+}
